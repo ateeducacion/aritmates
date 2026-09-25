3c217474d1af4195716479246fed360b9bd4cffa
Ernesto Serrano <info@ernesto.es>
2026-09-25 17:06:37 +0100

    docs: use institutional ATE contact email

--- src/utils.js
+++ src/utils.js
@@ -16,7 +16,7 @@ function sameElements(array1, array2) {
  * Distintas funciones que se pueden ser utilidad
  * 
  * @author Fernando Ramirez <fernando.ramirez@altia.es>
- * @author Área de Tecnología Educativa (versión simplificada 1.3+)
+ * @author Área de Tecnología Educativa <ate.educacion@gobiernodecanarias.org> (versión simplificada 1.3+)
  * @version 1.0.0-rc1
  *
  * @class Utils
